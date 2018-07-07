6bf8cf2d77a1528ffbff1947fb2eb23329636964
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-07-07 15:36:17 -0500

    vuepress added

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'TaxJar Style Guide',
+    description: 'Just playing around',
+    // .vuepress/config.js
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+  }
